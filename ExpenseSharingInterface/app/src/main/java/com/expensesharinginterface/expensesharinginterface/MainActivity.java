89a9a355ffc9821b2452a715e48bbcb0886f87af
pmukher <pmukher@ncsu.edu>
2016-02-13 17:35:11 -0500

    Created objects for buttons and the action listeners

--- ExpenseSharingInterface/app/src/main/java/com/expensesharinginterface/expensesharinginterface/MainActivity.java
+++ ExpenseSharingInterface/app/src/main/java/com/expensesharinginterface/expensesharinginterface/MainActivity.java
@@ -5,16 +5,60 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
 import android.view.View;
+import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button one, two, three, four, five, six, seven, eight, nine, zero, decimal, delete, reset, next;
+    private EditText input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        one=(Button) findViewById(R.id.bt1);
+        two=(Button) findViewById(R.id.bt2);
+        three=(Button) findViewById(R.id.bt3);
+        four=(Button) findViewById(R.id.bt4);
+        five=(Button) findViewById(R.id.bt5);
+        six=(Button) findViewById(R.id.bt6);
+        seven=(Button) findViewById(R.id.bt7);
+        eight=(Button) findViewById(R.id.bt8);
+        nine=(Button) findViewById(R.id.bt9);
+        zero=(Button) findViewById(R.id.bt10);
+        decimal=(Button) findViewById(R.id.btDecimal);
+        delete=(Button) findViewById(R.id.btReset);
+        reset=(Button) findViewById(R.id.btDelete);
+        next=(Button) findViewById(R.id.btNext);
+        input=(EditText) findViewById(R.id.amount);
+        try
+        {
+            one.setOnClickListener(this);
+            two.setOnClickListener(this);
+            three.setOnClickListener(this);
+            four.setOnClickListener(this);
+            five.setOnClickListener(this);
+            six.setOnClickListener(this);
+            seven.setOnClickListener(this);
+            eight.setOnClickListener(this);
+            nine.setOnClickListener(this);
+            zero.setOnClickListener(this);
+            decimal.setOnClickListener(this);
+            delete.setOnClickListener(this);
+            reset.setOnClickListener(this);
+            next.setOnClickListener(this);
+
+        }
+        catch(Exception e)
+        {
+
+        }
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -33,6 +77,12 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View arg0)
+    {
+
     }
 
     @Override
