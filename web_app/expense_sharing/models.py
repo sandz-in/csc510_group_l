edb3eb6a2c276f2dc982462a9111962edb1a9589
Sandeep Samdaria <sandyridgeracer@gmail.com>
2016-02-13 22:16:34 -0500

    #7 User model for the application

--- web_app/expense_sharing/models.py
+++ web_app/expense_sharing/models.py
@@ -1,5 +1,51 @@
 from __future__ import unicode_literals
+from django.contrib.auth.base_user import AbstractBaseUser
 
 from django.db import models
 
 # Create your models here.
+from expense_sharing.utils import MyUserManager
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    first_name = models.CharField(default=False, max_length=32)
+    last_name = models.CharField(default=False, max_length=32)
+    cell_no = models.CharField(default=False, max_length=32)
+
+    objects = MyUserManager
+
+    USERNAME_FIELD = 'email'
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def __str__(self):  # __unicode__ on Python 2
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
