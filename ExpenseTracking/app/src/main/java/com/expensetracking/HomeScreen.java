d613c6c9eda2c9f4038ae4f554346f22bf3284d3
Sandeep Samdaria <sandyridgeracer@gmail.com>
2016-02-27 16:43:29 -0500

    #27: Login screen

---
+++ ExpenseTracking/app/src/main/java/com/expensetracking/HomeScreen.java
@@ -0,0 +1,65 @@
+package com.expensetracking;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import org.json.JSONObject;
+
+
+public class HomeScreen extends Activity {
+    EditText un, pw;
+    TextView error;
+    Button ok,register;
+    private String resp;
+    private String errorMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home_screen);
+        un = (EditText) findViewById(R.id.et_un);
+        pw = (EditText) findViewById(R.id.et_pw);
+        ok = (Button) findViewById(R.id.btn_login);
+        register = (Button) findViewById(R.id.btn_reg);
+        error = (TextView) findViewById(R.id.tv_error);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.logout) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onRegButtonClick (View view)
+    {
+    }
+}
